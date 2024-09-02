933475c98e9c0f84ff9bd830bcdc544a7c619dc0
Lukhanyo Radebe <khanyoradebe46@gmail.com>
2024-09-02 22:41:04 +0200

    modified

--- Functions_and_Graphing.py
+++ Functions_and_Graphing.py
@@ -1,20 +1,17 @@
 # Run 'pip install matplotlib' on your CLI before running this program
 #print('x \t y')
 
-#for x in range(11):
-#    y = 4 * x + 3 # the linear function 
-#    print(f'{x} \t {y}')
-
 # Basic blank graph
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 
 # Define dimensions of graph
-xmin = -10
-xmax = 10
-ymin = -10
-ymax = 10
+xmin = int(input('Enter xmin dimension: '))
+xmax = int(input('Enter xmax dimension: '))
+ymin = int(input('Enter ymin dimension: '))
+ymax = int(input('Enter ymax dimension: '))
 plt.axis([xmin, xmax ,ymin ,ymax]) # window size
+print()
 
 # Display axis lines
 plt.plot([xmin,xmax],[0,0],'b') # blue x axis
@@ -22,10 +19,13 @@ plt.plot([0,0],[ymin,ymax],'b') # blue y axis
 
 # Plotting
 plot_t = int(input('Enter number of plots:  ')) # like how many times are you going to plot points on the graph
+print()
 for p in range(plot_t):
+    p += 1
+    print(f'{p} plot')
     x_plot = int(input('Enter x value: '))
     y_plot = int(input('Enter y value: '))
-
     plt.plot([x_plot],[y_plot], 'ro')
+    print()
 
 plt.show()
