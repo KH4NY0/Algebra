7fd5ec80d1c43cd1bbd4f494eb28f0d94e6635de
Lukhanyo Radebe <khanyoradebe46@gmail.com>
2024-09-02 15:47:37 +0200

    modified

--- Solve_for_X.py
+++ Solve_for_X.py
@@ -1,15 +1,22 @@
-# Remember to run "pip install sympy" on your CLI before running this program
+# Run "pip install sympy" on your CLI before running this program
 from sympy import * 
-
 
 x = symbols('x')
 
-# we are always assuming that y = 0 , but i'll figure out later on how to declare the value of 'y' in the equation
-eq = input('Enter equation: y =  ')
+user_value = input('Enter value of y: ')
+eq = input(f'Enter equation: {user_value}  =  ')
+y = int(user_value)
 
-solution = solve(eq,x)
-print(solution)
+if y >= 0:
+    solution1 = f'{eq} - {user_value}' 
+    solution2 = solve(solution1,x)
+    for s in solution2:
+        print(f'x = {s}')
 
-solution = solve(eq,x)
-for s in solution:
-    print(f'x = {s}')
+else:
+    solution1 = f'{eq} + {user_value}' 
+    solution2 = solve(solution1,x)
+    for s in solution2:
+        print(f'x = {s}')
+    
+
